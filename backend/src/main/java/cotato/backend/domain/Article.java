4b4e518a8c7945b85a336f7d1f2b5c9988c8ec9d
0-x-14 <0x14@daum.net>
2024-07-27 00:37:13 +0900

    feat: Article 추가

---
+++ backend/src/main/java/cotato/backend/domain/Article.java
@@ -0,0 +1,39 @@
+package cotato.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Article {
+    @Id
+    @Column(name = "article_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ArticleId;
+
+    private String image;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String detail;
+
+    @Column(nullable = false)
+    private Date expireDate;
+
+    @Column(nullable = false)
+    private LocalDateTime createDate;
+
+    // Sender Receiver 필요
+    @ManyToOne
+    private Member member;
+}
