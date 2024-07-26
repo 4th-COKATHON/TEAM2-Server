9331efdb6c6567659036abc388178faee9dee0d9
hl3470 <hl3470@naver.com>
2024-07-27 03:59:02 +0900

    feat: articleService 1차구현

--- backend/src/main/java/cotato/backend/repository/ArticleRepository.java
+++ backend/src/main/java/cotato/backend/repository/ArticleRepository.java
@@ -1,17 +1,24 @@
 package cotato.backend.repository;
 
-import cotato.backend.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cotato.backend.domain.Article;
+import cotato.backend.domain.Member;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-//    // 기간이 만료된 글들을 불러옴
-//    List<Article> findByExpiredAtBefore(LocalDateTime todayDate);
-//
-//    // 기간이 지나지 않은 글들을 불러옴
-//    List<Article> findByExpiredAtAfter(LocalDateTime todayDate);
-//    List<Article> findByExpiredAtAfterAndSenderId(LocalDateTime todayDate, Long senderId);
+	// 기간이 만료된 글들을 불러옴
+	Page<Article> findByExpiredAtBeforeAndSender_LoginId(LocalDateTime todayDate, String senderId, Pageable pageable);
+
+	Page<Article> findByExpiredAtBeforeAndSender_LoginIdAndReceiver_LoginId(LocalDateTime todayDate, String senderId, String receiverId, Pageable pageable);
+
+	// 기간이 지나지 않은 글들을 불러옴
+	Page<Article> findByExpiredAtAfterAndSender_LoginId(LocalDateTime todayDate, String senderId, Pageable pageable);
+
+	Page<Article> findByExpiredAtAfterAndSender_LoginIdAndReceiver_LoginId(LocalDateTime todayDate, String senderId, String receiverId, Pageable pageable);
 
 }
--- backend/src/main/java/cotato/backend/service/ArticleService.java
+++ backend/src/main/java/cotato/backend/service/ArticleService.java
@@ -1,29 +1,53 @@
 package cotato.backend.service;
 
-import cotato.backend.domain.Article;
-import cotato.backend.dto.GetNotExpiredArticleResponseDTO;
-import cotato.backend.repository.ArticleRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cotato.backend.dto.ArticleDTO.TimeCapsuleItem;
+import cotato.backend.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
 
-    private final ArticleRepository articleRepository;
+	private final int PAGE_SIZE = 3;
 
-//    // 내가 나에게 작성한 글들 중 기간이 지나지 않은 글들을 불러옴
-//    public GetNotExpiredArticleResponseDTO getExpiredArticleService(int filter) {
-//        LocalDateTime now = LocalDateTime.now();
-//        List<Article> articles = articleRepository.findByExpiredAtAfter(now);
-//        // 리턴값 - expiredAt
-//
-//        if (filter == 1) {
-//            return new GetNotExpiredArticleResponseDTO();
-//        }
-//    }
+	private final ArticleRepository articleRepository;
+
+	public Page<TimeCapsuleItem> getTimeCapsules(String loginId, boolean lock, boolean self, int page) {
+
+		Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+		LocalDateTime now = LocalDateTime.now();
+
+		//1. 아직 잠겨 있으면서, 내가 쓴 글
+		//2. 아직 잠겨 있으면서, 남들이 써준 글
+		//3. 잠금이 풀렸으면서, 내가 쓴 글
+		//4. 잠금이 풀렸으면서, 남들이 써준 글
+		if (lock && self) {
+			articleRepository.findByExpiredAtBeforeAndSender_LoginIdAndReceiver_LoginId(now, loginId, loginId,
+				pageable);
+		} else if (lock && !self) {
+			articleRepository.findByExpiredAtBeforeAndSender_LoginId(now, loginId, pageable);
+		} else if (!lock && self) {
+			articleRepository.findByExpiredAtAfterAndSender_LoginIdAndReceiver_LoginId(now, loginId, loginId, pageable);
+		} else {
+			articleRepository.findByExpiredAtAfterAndSender_LoginId(now, loginId, pageable);
+		}
+
+		List<TimeCapsuleItem> objects = new ArrayList<>();
+
+		return new PageImpl<>(objects, pageable, 1);
+	}
+
 }
