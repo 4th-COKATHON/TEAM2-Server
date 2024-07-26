126c237abc63ffdd59af2a672fe3d2e4e1abc668
hl3470 <hl3470@naver.com>
2024-07-26 23:22:29 +0900

    feat: redisProperties 추가

--- backend/src/main/java/cotato/backend/common/properties/conf/PropertyConfig.java
+++ backend/src/main/java/cotato/backend/common/properties/conf/PropertyConfig.java
@@ -4,11 +4,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 import cotato.backend.common.properties.JwtProperties;
+import cotato.backend.common.properties.RedisProperties;
 
 // 전역적으로 사용되는 상수
 @Configuration
 @EnableConfigurationProperties(value = {
 	JwtProperties.class,
+	RedisProperties.class
 })
 public class PropertyConfig {
 }
